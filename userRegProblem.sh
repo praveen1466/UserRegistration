5b238cee919c4b727e3b1ebb9a16b975ad3b5630
praveen1466 <praveensknl7@gmail.com>
2020-11-25 19:34:08 -0800

    Added: Firstname of RegX is added

--- userRegProblem.sh
+++ userRegProblem.sh
@@ -1,1 +1,12 @@
 echo "Welcome to Regx for User Registration"
+
+echo "Enter the name"
+read firstName
+Regex="^[A-Z][a-z]{2,}$"
+
+if [[ $firstName =~ $Regex ]]
+then
+	echo "firstName is Valid"
+else
+	echo "firstName is Invalid.. Type Another name"
+fi
